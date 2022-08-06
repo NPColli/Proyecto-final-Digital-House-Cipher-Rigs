3cdcd83b1c44b71ea4965d8707cb3c9fb0aa27cd
albruno-dev <alansergiobruno@gmail.com>
2022-08-06 06:16:18 -0300

    form contacto - video backgrounds

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,19 @@
+let db = require("../../database/models");
+
 const controller = {
     index: (req, res) => {
         return res.render('index');
-    },
-    contacto: (req, res) => {
+    } ,
+        contacto: function(req, res){
+            db.Contact.create ({
+                nombre:req.body.nombre ,
+                email: req.body.email,
+                asunto: req.body.asunto,
+                mensaje:req.body.mensaje ,
+            })
         return res.render('contacto');
-    },
+        }
+    ,
     nosotros: (req, res) => {
         return res.render('nosotros');
     }
