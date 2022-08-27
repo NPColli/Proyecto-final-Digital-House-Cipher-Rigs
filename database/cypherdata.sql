eebae7154d0eab320b246655cc5f03548d626323
albruno-dev <alansergiobruno@gmail.com>
2022-08-26 23:09:56 -0300

    data

---
+++ database/cypherdata.sql
@@ -0,0 +1,12 @@
+INSERT INTO categories (description)
+VALUES ('rigs');
+
+INSERT INTO categories (description)
+VALUES ('asic');
+
+
+
+SELECT * FROM users
+SELECT * FROM contacto
+
+SELECT * FROM asics
